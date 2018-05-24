abe859c856b9bb13e3b1d58a2e24f62b95fb9a25
mihirsathe <m1sathe@ucsd.edu>
2018-05-23 17:50:46 -0700

    Working image upload and capture functionality

---
+++ app/src/main/java/com/example/mihir/iopv3/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.mihir.iopv3;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int SELECT_FILE_REQUEST_CODE = 200;
+    private Uri logURI = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button takePicBtn = findViewById(R.id.takePicBtn);
+        Button choosePicBtn = findViewById(R.id.choosePicBtn);
+        takePicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                            MY_CAMERA_REQUEST_CODE);
+                }
+                else{
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    File tempIm = getImFile();
+                    if (tempIm != null) {
+                        Uri photoURI = FileProvider.getUriForFile(getApplicationContext(),BuildConfig.APPLICATION_ID, tempIm);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, MY_CAMERA_REQUEST_CODE);
+                        logURI = photoURI;
+
+                    }
+                }
+
+                // Todo if  no permissions get permissions
+                // Todo if file created, call camera intent
+            }
+        });
+        choosePicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Todo if  no permissions get permissions
+                // Todo if permissions granted, call file picker intent
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, SELECT_FILE_REQUEST_CODE);
+                }
+                else{
+                    /* Choose an image from Gallery */
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_FILE_REQUEST_CODE);
+                }
+            }
+        });
+
+
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if(grantResults.length != 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "camera permission granted, please retry", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        if (requestCode == SELECT_FILE_REQUEST_CODE ){
+            if(grantResults.length != 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "read permission given, please retry", Toast.LENGTH_LONG).show();
+                }
+            }
+            else{
+                Toast.makeText(this, "read permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private File getImFile(){
+        File tempIm = null;
+        try {
+            tempIm = File.createTempFile("img",".bmp");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tempIm;
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if( resultCode == Activity.RESULT_OK) {
+            //if user chooses to take a picture with the camera
+            if (requestCode == MY_CAMERA_REQUEST_CODE) {
+                Intent i = new Intent(this, loadingScreenAct.class);
+                i.putExtra("photoUri", logURI);
+                startActivity(i);
+            }
+            else if (requestCode == SELECT_FILE_REQUEST_CODE) {
+                Uri photoUri = data.getData();
+                Intent i = new Intent(this, loadingScreenAct.class);
+                i.putExtra("photoUri", photoUri);
+                startActivity(i);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mihir/iopv3/loadingScreenAct.java
@@ -0,0 +1,39 @@
+package com.example.mihir.iopv3;
+
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+
+public class loadingScreenAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+        findViewById(R.id.mainSpinner1).setVisibility(View.VISIBLE);
+        Uri photoUri =   this.getIntent().getParcelableExtra("photoUri");
+        ContentResolver cr = getApplicationContext().getContentResolver();
+        Bitmap bmp = null;
+        try {
+            bmp = BitmapFactory.decodeStream(cr.openInputStream(photoUri));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ImageView mImg;
+        mImg = findViewById(R.id.imageView);
+        mImg.setImageBitmap(bmp);
+        Log.i("Photo saved?", String.valueOf(bmp));
+
+        // TODO Insert Processing Code
+        // Todo Get result number and pass to show result activity
+
+    }
+}
