46d7be19ba6dfe5234f2c85712eb5676c82758f9
mihirsathe <m1sathe@ucsd.edu>
2018-05-30 19:16:10 -0700

    Loadingscreen updates

--- app/src/main/java/com/example/mihir/iopv3/loadingScreenAct.java
+++ app/src/main/java/com/example/mihir/iopv3/loadingScreenAct.java
@@ -24,6 +24,8 @@ import java.util.Arrays;
 
 import static java.lang.Math.log;
 import static org.opencv.core.Core.BORDER_REFLECT;
+import static org.opencv.imgproc.Imgproc.MORPH_ELLIPSE;
+import static org.opencv.imgproc.Imgproc.MORPH_GRADIENT;
 
 public class loadingScreenAct extends AppCompatActivity {
 
@@ -49,7 +51,7 @@ public class loadingScreenAct extends AppCompatActivity {
         hist.convertTo(hist, CvType.CV_32S);
         hist.get(0, 0, histogram);
         for (int i = 0; i < grayMax; i++) {
-            entropy += entropy_tab[histogram[i]];
+            entropy -= entropy_tab[histogram[i]];
         }
         return entropy;
     }
@@ -83,7 +85,6 @@ public class loadingScreenAct extends AppCompatActivity {
         Mat entropy = new Mat(gray.size(), CvType.CV_64FC1);
         // 7x7 neighborhood
         int box_size = 7;
-        final Size filtSize = new Size(box_size, box_size);
 
         // Pad image to cover edges
         int pad = (box_size - 1) / 2;
@@ -101,17 +102,18 @@ public class loadingScreenAct extends AppCompatActivity {
             }
         }
         entropy.put(0, 0, data);
-        //Todo normalize the entropy values
-        Core.normalize(entropy, entropy, 0, 255, Core.NORM_MINMAX, CvType.CV_8UC1);
+        // normalize the entropy values
+        Core.normalize(entropy, entropy, 0, 255, Core.NORM_MINMAX, CvType.CV_32FC1);
 
-        // Todo convert to float32
-        //Mat floatGray = new Mat();
-        //detectedEdges.convertTo(floatGray, CvType.CV_32S);
 
-        //Todo morphological dilation
-        //final Size strelSize = new Size(10,10);
-        //Mat element = Imgproc.getStructuringElement( MORPH_ELLIPSE, strelSize);
-        //Imgproc.morphologyEx(detectedEdges,detectedEdges,MORPH_GRADIENT,element);
+        // morphological dilation
+        final Size strelSize = new Size(8, 8);
+        Mat element = Imgproc.getStructuringElement(MORPH_ELLIPSE, strelSize);
+        Imgproc.morphologyEx(entropy, entropy, MORPH_GRADIENT, element);
+
+        entropy.convertTo(entropy, CvType.CV_8UC1);
+        // Threshold image to binary
+        Imgproc.threshold(entropy, entropy, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU);
 
         // Convert image and display in imageview
         Bitmap bam = Bitmap.createBitmap(entropy.cols(), entropy.rows(), Bitmap.Config.ARGB_8888);
@@ -120,14 +122,16 @@ public class loadingScreenAct extends AppCompatActivity {
         ImageView mImg;
         mImg = findViewById(R.id.imageView);
         mImg.setImageBitmap(bam);
-/*        new Handler().postDelayed(new Runnable() {
+/*
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent showResultActivity = new Intent(getApplicationContext(), showResultActivity.class);
                 showResultActivity.putExtra("pressureVal", 21);
                 startActivity(showResultActivity);
             }
-        }, 3500);*/
+        }, 3500);
+*/
 
     }
 }
